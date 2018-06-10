6a6e20827e0eef14e81a1b349258d9d35613d570
da-luddite <da_luddite@yahoo.com>
2018-06-10 22:47:17 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/odiousenterprises/spring/OdiousenterprisesSpring1Application.java
@@ -0,0 +1,12 @@
+package com.odiousenterprises.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdiousenterprisesSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdiousenterprisesSpring1Application.class, args);
+	}
+}
---
+++ src/test/java/com/odiousenterprises/spring/OdiousenterprisesSpring1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.odiousenterprises.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OdiousenterprisesSpring1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
