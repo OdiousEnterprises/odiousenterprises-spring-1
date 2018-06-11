0d80067284542ec852a1d4912c4c62dd4262d590
Eric Hamlin <da_luddite@yahoo.com>
2018-06-10 19:59:32 -0700

    dependencies updated now?

--- src/main/java/com/odiousenterprises/spring/OdiousenterprisesSpring1Application.java
+++ src/main/java/com/odiousenterprises/spring/OdiousenterprisesSpring1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OdiousenterprisesSpring1Application {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OdiousenterprisesSpring1Application.class, args);
 	}
