4e8ac6f2790c8dd17d35970cd0cca6dbd4388365
Eric Hamlin <da_luddite@yahoo.com>
2018-06-10 19:47:29 -0700

    clean install?

--- src/main/java/com/odiousenterprises/spring/OdiousenterprisesSpring1Application.java
+++ src/main/java/com/odiousenterprises/spring/OdiousenterprisesSpring1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OdiousenterprisesSpring1Application {
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OdiousenterprisesSpring1Application.class, args);
